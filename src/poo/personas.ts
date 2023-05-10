56b6a4b531ab51a78efe908c04a8cfd16712317b
Guillermo Perez <guilleps082020@gmail.com>
2023-05-09 20:32:35 -0500

    Avance antes de la exposicion de la solucion del profesor

--- src/poo/personas.ts
+++ src/poo/personas.ts
@@ -11,14 +11,16 @@ class Hombre implements IPersona{
         this.apellido = apellido ?? '';
     }
 
+    //Metodo de la interfaz IPersonas
     nombreCompleto(): string {
         const mensaje = `Tu nombre completo es ${this.nombre} y tu apellido es ${this.apellido} y eres hombre`;
         console.log(mensaje)
         return mensaje
     }
 
-
-
+    nombre_completo(): string{
+        console.log(`Tu nombre completo es ${this.nombre} ${this.apellido}`)
+    }
 }
 
 class Mujer implements IPersona{
