8a9e0ddf1cd0b26aa1f79e08090485685a6ca8a6
Guillermo Perez <guilleps082020@gmail.com>
2023-05-09 20:18:34 -0500

    Avance 8:18PM

--- src/poo/personas.ts
+++ src/poo/personas.ts
@@ -2,7 +2,14 @@ import { IPersona } from "./interface_IPersona";
 
 class Hombre implements IPersona{
 
-    constructor(public nombre?: string, public apellido?: string, public edad: number){}
+    public nombre: string; 
+    public apellido: string;
+    public edad!: number;
+
+    constructor(nombre?: string, apellido?: string){
+        this.nombre = nombre ?? '';
+        this.apellido = apellido ?? '';
+    }
 
     nombreCompleto(): string {
         const mensaje = `Tu nombre completo es ${this.nombre} y tu apellido es ${this.apellido} y eres hombre`;
